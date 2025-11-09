dabb83f0425aab3e79022231158a3a63ac077d8a
AndiRoihan <roihanaqilah72@gmail.com>
2025-11-10 01:35:27 +0800

    feat: update web

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -9,23 +9,37 @@ const navBarLinks = [
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
+    section: "Info Lebih Lanjut",
     links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
+      { name: "Alamat: Kompleks Ruko Cakalang Indah No 3 V, Totaka, Ujung Tanah, Kota Makassar", url: "https://maps.app.goo.gl/Bngi2HiDtveXCDnq6" },
+      { name: "Instagram: @rotiaromahs", url: "https://www.instagram.com/rotiaromahs/" },
+      { name: "WhatsApp: 0852-9991-0439", url: "https://wa.me/6285299910439" },
     ],
   },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
+  // {
+  //   section: "Site Map",
+  //   links: [
+  //     { name: "Home", url: "#home" },
+  //     { name: "Products", url: "#products" },
+  //     { name: "About Us", url: "#about-us" },
+  //     { name: "Paket", url: "#paket" },
+  //     { name: "FAQ", url: "#faq" },
+  //   ],
+  // },
 ];
+
+// const siteMaps = [
+//   {
+//     section: "Site Map",
+//     links: [
+//       { name: "Home", url: "#home" },
+//       { name: "Products", url: "#products" },
+//       { name: "About Us", url: "#about-us" },
+//       { name: "Paket", url: "#paket" },
+//       { name: "FAQ", url: "#faq" },
+//     ],
+//   },
+// ];
 // An object of links for social icons
 const socialLinks = {
   facebook: "https://www.facebook.com/",
